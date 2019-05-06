4caf097ebb2d0f6a5d74f0748f63bdbf90980bc4
Andres Barro <andresbarroe+git@gmail.com>
2019-05-05 23:01:28 -0500

    scrapping
    Signed-off-by: Andres Barro <andresbarroe+git@gmail.com>

--- extra_scrap.py
+++ extra_scrap.py
@@ -3,9 +3,18 @@
 import pandas as pd
 import requests
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+key = 'AIzaSyD7aumbqlL9DbCOsh42wPLtZnDFIQzKTkc'
 videos = pd.read_csv('./data/USvideos.csv')
-ids = videos['video_id']
-# print('[')
-# response = requests.get('https://www.googleapis.com/youtube/v3/videos?id=9bZkp7q19f0,kjyeCdd-dl8&part=contentDetails&key=AIzaSyD7aumbqlL9DbCOsh42wPLtZnDFIQzKTkc')
-# print(response.text, ',')
-# print(']')
+
+print('[')
+for i in chunks(videos['video_id'], 50):
+    url = 'https://www.googleapis.com/youtube/v3/videos?id=' + ','.join(i) + '&part=contentDetails&key=' + key
+    response = requests.get(url)
+    print(response.text, ',')
+#     break
+print(']')
